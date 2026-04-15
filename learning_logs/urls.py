c419b2803eff9454dde417f270f3a77472248b1b
37charlessilva <37charlessilva@gmail.com>
2026-04-15 17:41:47 -0300

    formulario de assuntos aedcionando

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,7 +11,11 @@ urlpatterns = [
     # Mostra  todos os assuntos
     path('topics/', views.topics, name='topics'),
     # Página de detalhes para um único assunto
-    path('topics/<int:topic_id>/', views.topic, name = 'topic'),
-    # página para adicionar um novo assunto
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Página para adicionar um novo assunto
     path('new_topic/', views.new_topic, name='new_topic'),
+    # Página para adicionar uma nova entrada
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Página para editar uma entrada
+    #path('edit_entry/<int:entry_id', views.edit_entry, name='edit_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 def index(request): 
@@ -41,3 +41,23 @@ def new_topic(request):
         
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Acrescenta uma nova entrada para um assunto em particular."""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulario em branco
+        form = EntryForm()
+
+    else:
+        # Dados de POST submetido; processa os dados
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
